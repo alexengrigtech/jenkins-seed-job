67bc621596990d28c0fca50e90dbd0cd19f1fbba
Grig Alex <alexengrigdev@gmail.com>
2022-12-24 21:48:32 +0300

    Create githubUserFolder.groovy

---
+++ jobs/githubUserFolder.groovy
@@ -0,0 +1,10 @@
+organizationFolder('github-alexengrig-folder') {
+    displayName('GitHub alexengrig')
+
+    description('Organization-folder for GitHub alexengrig')
+
+    organizations {
+        repoOwner('alexengrig')
+        includes('jenya-application')
+    }
+}
